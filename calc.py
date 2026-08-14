a83555a860b20ae8e482a06a57180546ae52a72c
ForAlex23 <110073106+ForAlex23@users.noreply.github.com>
2026-08-14 13:55:42 +0500

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,16 +1,24 @@
-print("This calculator will add two numbers together.")
-number1 = float(input("Enter the first number: "))
-number2 = float(input("Enter the second number: "))
-operation = input("Enter the operation (+, -, *, /): ")
-if operation == "+":
-    result = number1 + number2
-elif operation == "-":
-    result = number1 - number2
-elif operation == "*":
-    result = number1 * number2
-elif operation == "/":
-    result = number1 / number2
-else:
-    print("Invalid operation.")
-print(f"The result is {result}.")
-input("Press Enter to continue...")
+while True:
+    number1 = input("Enter expression (or q to quit), enter the first number: ")
+    if number1.lower() == "q":
+        break
+    number1 = float(number1)
+    number2 = float(input("Enter the second number: "))
+    operation = input("Enter the operation (+, -, *, /):")
+    if operation == "+":
+        result = number1 + number2
+        print(f"The result is {result}.")
+    elif operation == "-":
+        result = number1 - number2
+        print(f"The result is {result}.")
+    elif operation == "*":
+        result = number1 * number2
+        print(f"The result is {result}.")
+    elif operation == "/":
+        if number2 == 0:
+            print("Error: Division by zero.")
+            continue
+        result = number1 / number2
+        print(f"The result is {result}.")
+    else:
+        print("Invalid operation.")
